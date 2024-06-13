7e6f7b3a06091e8cee6899d5d373efb50349896b
Carlo Eduardo <carloedu010@gmail.com>
2024-06-13 16:46:07 -0300

    Add script

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
-// f92c6f89153f558cd38b5d86e8d56f41
+
+
+async function ConsultarTempo(){
+
+    const container = document.querySelector("main")
+    const error = document.querySelector(".error")
+    const weather = document.querySelector(".weather")
+    const city_name = document.querySelector("#name")
+    const image = document.querySelector("#image")
+    const temperature = document.querySelector("#temperature")
+    const description = document.querySelector("#description")
+    const humidity = document.querySelector("#humidity")
+    const wind = document.querySelector("#wind")
+    const key = 'f92c6f89153f558cd38b5d86e8d56f41'
+    const city = document.querySelector("#search").value
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt_br&units=metric&appid=${key}`
+
+    let response = await fetch(URL)
+    let data = await response.json()
+    
+    if (data.cod == '404'){
+        weather.style.display = "none"
+        error.style.display = 'block'
+        return
+    }
+    error.style.display = "none"
+    weather.style.display = 'block'
+    city_name.innerText = data.name
+    temperature.innerHTML = `${parseInt(data.main.temp)}<span>ºC</span>`
+    description.innerText = data.weather[0].description
+    humidity.innerText = `${data.main.humidity}%`
+    wind.innerText = `${parseInt(data.wind.speed)}Km/h`
+    switch (data.weather[0].main){
+        case "Clear":
+            image.src = "img/clear.png"
+            break
+        case "Clouds":
+            image.src = "img/cloud.png"
+            break
+        case "Mist":
+            image.src = "img/mist.png"
+            break
+        case "Rain":
+            image.src = "img/rain.png"
+            break
+        case "Snow":
+            image.src = "img/snow.png"
+            break
+        default:
+            image.src = ""
+    }
+}
